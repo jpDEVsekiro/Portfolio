6c6633fee99d7bb969c956aa314355d0d1088229
João Pedro <devjpdesenvolver@gmail.com>
2023-07-30 19:41:13 -0300

    scoll controller

--- lib/src/application/controllers/home_controller.dart
+++ lib/src/application/controllers/home_controller.dart
@@ -7,6 +7,8 @@ class HomeController extends GetxController {
   final Uri _gitHub = Uri.parse('https://github.com/jpDEVsekiro');
   final Uri _linkedin =
       Uri.parse('https://www.linkedin.com/in/jo%C3%A3o-pedro--/');
+  final Uri _whatsApp = Uri.parse('https://wa.me/5511959991724');
+  final Uri _email = Uri.parse('mailto:devjpdesenvolver@gmail.com');
 
   void goToGitHub() {
     launchUrl(_gitHub);
@@ -14,5 +16,21 @@ class HomeController extends GetxController {
 
   void goToLinkedIn() {
     launchUrl(_linkedin);
+  }
+
+  void goToWhatsApp() {
+    launchUrl(_whatsApp);
+  }
+
+  void goToEmail() {
+    launchUrl(_email);
+  }
+
+  void scrollAboutMe() {
+    scrollController.animateTo(
+      2500,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
   }
 }
